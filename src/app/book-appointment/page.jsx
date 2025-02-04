d864f494723d4ae105249886a4c57e351383bf0a
Shubham Mankar <mankarshubham81@gmail.com>
2025-02-04 21:40:09 +0530

    added bookAppoinment

---
+++ src/app/book-appointment/page.jsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+
+export default function BookAppointment() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-blue-100 flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <Head>
+        <title>Book Appointment - Homeopathy Doctor</title>
+        <meta name="description" content="Easily book an appointment with your homeopathy doctor." />
+      </Head>
+
+      <div className="max-w-lg w-full space-y-4 bg-white p-10 rounded-2xl shadow-xl transition-transform transform hover:scale-105">
+
+        {/* Embedded Google Form */}
+        <div className="mt-4 overflow-hidden rounded-lg border border-gray-300 shadow-lg">
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSf3soxJKQCkQqABe1pauweo9lufM0oXnM7GIpC1zFmgmlNnAg/viewform?usp=dialog"
+            width="100%"
+            height="900"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+            title="Book Appointment Form"
+            className="rounded-lg"
+          >
+            Loading…
+          </iframe>
+        </div>
+      </div>
+    </div>
+  );
+}
