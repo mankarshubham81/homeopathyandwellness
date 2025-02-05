c621a8a9282d569a4e144bbb998a2af86708fce0
Shubham Mankar <mankarshubham81@gmail.com>
2025-02-05 17:12:46 +0530

    book appointment tweaks

--- src/app/book-appointment/page.jsx
+++ src/app/book-appointment/page.jsx
@@ -2,13 +2,13 @@ import Head from 'next/head';
 
 export default function BookAppointment() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-blue-100 flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-blue-100 flex flex-col items-center justify-center py-1 px-1 sm:px-6 lg:px-2">
       <Head>
         <title>Book Appointment - Homeopathy Doctor</title>
         <meta name="description" content="Easily book an appointment with your homeopathy doctor." />
       </Head>
 
-      <div className="max-w-lg w-full space-y-4 bg-white p-10 rounded-2xl shadow-xl transition-transform transform hover:scale-105">
+      <div className="max-w-lg w-full  bg-white p-1 rounded-2xl shadow-xl transition-transform transform hover:scale-105">
 
         {/* Embedded Google Form */}
         <div className="mt-4 overflow-hidden rounded-lg border border-gray-300 shadow-lg">
